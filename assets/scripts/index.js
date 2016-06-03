e1d7284ce92c1eb847b6144acd2e8480972dbb8c
Filip Chalupa <chalupa.filip@gmail.com>
2016-06-03 21:38:00 +0200

    Init: basic version (show currently playing track)

---
+++ assets/scripts/index.js
@@ -0,0 +1,97 @@
+(function(){
+	var username = window.location.hash.substr(1)
+	if (!username) {
+		username = 'Ofecka_'
+	}
+
+	var track = document.getElementById('track')
+	var trackPrevious
+	var trackCurrent = track.querySelector('.track-item')
+	var trackProps = {}
+	var lastTrackId
+
+	function setText(el, text) {
+		var textNode = document.createTextNode(text)
+		el.innerHTML = ''
+		el.appendChild(textNode)
+	}
+
+	function getProps(el) {
+		return {
+			under: el.querySelector('.track-under'),
+			cover: el.querySelector('.track-cover'),
+			title: el.querySelector('.track-title'),
+			artist: el.querySelector('.track-artist'),
+			album: el.querySelector('.track-album')
+		}
+	}
+
+	function show(title, artist, album, cover) {
+		if (trackPrevious) {
+			trackPrevious.remove()
+		}
+		trackPrevious = trackCurrent
+		trackCurrent = trackPrevious.cloneNode(true)
+		trackCurrent.classList.add('view-new')
+
+		setTimeout(function(){
+			trackCurrent.classList.remove('view-new')
+			trackPrevious.classList.add('view-old')
+		}, 10)
+
+		track.appendChild(trackCurrent)
+
+		trackProps = getProps(trackCurrent)
+		trackProps.under.style.backgroundImage = 'url('+cover+')'
+		trackProps.cover.style.backgroundImage = trackProps.under.style.backgroundImage
+		setText(trackProps.title, title)
+		setText(trackProps.artist, artist)
+		setText(trackProps.album, album)
+	}
+
+	function getRecentUrl(username) {
+		return 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+username+'&limit=1&api_key=4bf2f3f683673b6f60730f65cf30cb28&format=json'
+	}
+
+	function getBySize(data, size, attr) {
+		var value = ''
+		for (var i = data.length - 1; i >= 0; i--) {
+			if (!value || data[i].size === size) {
+				value = data[i][attr]
+			}
+		}
+		return value
+	}
+
+	function update() {
+		fetch(getRecentUrl(username)).then(function(response){
+			return response.json()
+		}).then(function(data){
+			if (data.recenttracks.track.length == 0) {
+				throw new Error('No tracks')
+			}
+			var track = data.recenttracks.track[0]
+			if (lastTrackId !== track.url) {
+				lastTrackId = track.url
+				show(
+					track.name,
+					track.artist['#text'],
+					track.album['#text'],
+					getBySize(track.image, 'extralarge', '#text')
+				)
+			}
+
+			setTimeout(function(){
+				update()
+			}, 2000)
+		}).catch(function(err){
+			console.error(err)
+			setTimeout(function(){
+				update()
+			}, 5000)
+		})
+	}
+
+	update()
+
+})()
